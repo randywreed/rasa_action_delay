9b338dbcb962042960ec2a843923bb747f3946f8
randy reed <reedrw@appstate.edu>
2021-03-06 09:10:16 -0500

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,55 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class actionWait(Action):
+
+    def name(self) -> Text:
+        return "action_wait"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            import time
+            time.sleep(5)
+            dispatcher.utter_message(text="action complete")
+
+            return []
+
+class actionUtter(Action):
+
+    def name(self) -> Text:
+        return "action_utter"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+            dispatcher.utter_messaage(text="this is a separate action that will also ask you to wait")
+
+            return []
